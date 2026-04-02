24e27510c667e2911a1a2587d053e338d16f6fe1
PsychoLona <magictalk2016@gmail.com>
2026-04-02 13:27:48 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -746,4 +746,4 @@ def set_role_targets(role_id: int, targets: dict, db: Session = Depends(get_db),
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
